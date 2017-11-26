1a0726b0516aa9ca2a4ec060311243ad774faca8
buritto <ybiz177@gmail.com>
2017-11-26 14:37:48 +0500

    Add realization  for extension methods Using(CultureInfo) for different number darabase

--- ObjectPrinting/PropertyPrintingConfigExtention.cs
+++ ObjectPrinting/PropertyPrintingConfigExtention.cs
@@ -10,24 +10,35 @@ namespace ObjectPrinting
 {
     public static class PropertyPrintingConfigExtention
     {
+        private static PrintingConfig<TOwner> AddCulture<TOwner>(PrintingConfig<TOwner> printingConfig, CultureInfo culture,
+            Type type)
+        {
+            if (!printingConfig.CultureForDifferentNumberBase.ContainsKey(type))
+            {
+                printingConfig.CultureForDifferentNumberBase.Add(type, null);
+            }
+            printingConfig.CultureForDifferentNumberBase[type] = culture;
+            return printingConfig;
+        }
+
         public static PrintingConfig<TOwner> Using<TOwner>(this PropertyPrintingConfig<int, TOwner> propertyPrintingConfig, CultureInfo culture)
         {
-            return ((IPropertyPrintingConfig<int, TOwner>)propertyPrintingConfig).Config;
+            return AddCulture(((IPropertyPrintingConfig<int, TOwner>)propertyPrintingConfig).Config, culture, typeof(int));
         }
 
         public static PrintingConfig<TOwner> Using<TOwner>(this PropertyPrintingConfig<double, TOwner> propertyPrintingConfig, CultureInfo culture)
         {
-            return ((IPropertyPrintingConfig<double, TOwner>)propertyPrintingConfig).Config;
+            return AddCulture(((IPropertyPrintingConfig<double, TOwner>)propertyPrintingConfig).Config, culture, typeof(double));
         }
 
         public static PrintingConfig<TOwner> Using<TOwner>(this PropertyPrintingConfig<long, TOwner> propertyPrintingConfig, CultureInfo culture)
         {
-            return ((IPropertyPrintingConfig<long, TOwner>)propertyPrintingConfig).Config;
+            return AddCulture(((IPropertyPrintingConfig<long, TOwner>)propertyPrintingConfig).Config, culture, typeof(long));
         }
 
         public static PrintingConfig<TOwner> Using<TOwner>(this PropertyPrintingConfig<float, TOwner> propertyPrintingConfig, CultureInfo culture)
         {
-            return ((IPropertyPrintingConfig<float, TOwner>)propertyPrintingConfig).Config;
+            return AddCulture(((IPropertyPrintingConfig<float, TOwner>)propertyPrintingConfig).Config, culture, typeof(float));
         }
 
         public static PrintingConfig<TOwner> Clip<TOwner>(this PropertyPrintingConfig<string, TOwner> propertyPrintingConfig, CultureInfo culture)
