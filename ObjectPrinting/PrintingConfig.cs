6bd81b516310c0a5fcdb7e5e91a1b5c4f0077b58
buritto <ybiz177@gmail.com>
2017-11-26 20:16:08 +0500

    Add PrintToString

--- ObjectPrinting/PrintingConfig.cs
+++ ObjectPrinting/PrintingConfig.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace ObjectPrinting
@@ -11,27 +12,28 @@ namespace ObjectPrinting
     public class PrintingConfig<TOwner>
     {
 
-        public  List<Type> ExcludeTypes = new List<Type>();
-        public Dictionary<Type, Func<Type, string>> SerializationFuncsForDifferentType = new Dictionary<Type, Func<Type, string>>();
+        public List<Type> ExcludeTypes = new List<Type>();
+        public List<string> ExcludePropert = new List<string>();
+        public Dictionary<Type, Func<object, string>> SerializationFuncsForDifferentType = new Dictionary<Type, Func<object, string>>();
         public Dictionary<Type, CultureInfo> CultureForDifferentNumberBase = new Dictionary<Type, CultureInfo>();
-        public Dictionary<string, Func<Type, string>> SerializationFuncsForDifferentProperty = new Dictionary<string, Func<Type, string>>();
+        public Dictionary<string, Func<object, string>> SerializationFuncsForDifferentProperty = new Dictionary<string, Func<object, string>>();
+        private Dictionary<string, Func<string, string>> Clipper = new Dictionary<string, Func<string, string>>();
+        private Type[] finalTypes = new[]
+            {
+                typeof(int), typeof(double), typeof(float), typeof(string),
+                typeof(DateTime), typeof(TimeSpan)
+            };
+
 
         public string PrintToString(TOwner obj)
         {
             return PrintToString(obj, 0);
         }
 
-        private string PrintToString(object obj, int nestingLevel)
+        private string DefaultPrint(object obj, int nestingLevel)
         {
-            //TODO apply configurations
             if (obj == null)
                 return "null" + Environment.NewLine;
-
-            var finalTypes = new[]
-            {
-                typeof(int), typeof(double), typeof(float), typeof(string),
-                typeof(DateTime), typeof(TimeSpan)
-            };
             if (finalTypes.Contains(obj.GetType()))
                 return obj + Environment.NewLine;
 
@@ -48,6 +50,48 @@ namespace ObjectPrinting
             return sb.ToString();
         }
 
+        public string PrintToString(Object obj, int nestingLevel)
+        {
+            if (obj == null)
+                return "null" + Environment.NewLine;
+            if (finalTypes.Contains(obj.GetType()))
+                return obj + Environment.NewLine;
+            var identation = new string('\t', nestingLevel + 1);
+            var sb = new StringBuilder();
+            foreach (var property in obj.GetType().GetProperties())
+            {
+                if (CheckExclude(property))
+                    continue;
+                var serilizationProperty = ApplySerialization(property, obj);
+                if (Clipper.ContainsKey(property.Name))
+                {
+                    serilizationProperty = Clipper[property.Name](serilizationProperty.ToString());
+                }
+                sb.Append(identation + property.Name + " = " +
+                          PrintToString(serilizationProperty,
+                              nestingLevel + 1));
+            }
+            return sb.ToString();
+        }
+
+        private object ApplySerialization(PropertyInfo property, object obj)
+        {
+            if (SerializationFuncsForDifferentProperty.ContainsKey(property.Name))
+            {
+                return SerializationFuncsForDifferentProperty[property.Name](property.GetValue(obj));
+            }
+            if (SerializationFuncsForDifferentType.ContainsKey(property.PropertyType))
+            {
+                return SerializationFuncsForDifferentType[property.PropertyType](property.GetValue(obj));
+            }
+            return property.GetValue(obj); ;
+        }
+
+        private bool CheckExclude(PropertyInfo property)
+        {
+            return ExcludePropert.Contains(property.Name) || ExcludeTypes.Contains(property.PropertyType);
+        }
+            
         public PrintingConfig<TOwner> ExcludeType<TypeProperty>()
         {
             if (!ExcludeTypes.Contains(typeof(TypeProperty)))
@@ -62,7 +106,7 @@ namespace ObjectPrinting
 
         public PrintingConfig<TOwner> SerializingProperty<TypeProperty>(
             Expression<Func<TOwner, TypeProperty>> expression,
-            Func<Type, string> serializationMethod)
+            Func<TypeProperty, string> serializationMethod)
         {
             var propInfo =
                 ((MemberExpression) expression.Body)
@@ -71,17 +115,30 @@ namespace ObjectPrinting
             {
                 SerializationFuncsForDifferentProperty.Add(propInfo.Name, null);
             }
-            SerializationFuncsForDifferentProperty[propInfo.Name] = serializationMethod;
+            SerializationFuncsForDifferentProperty[propInfo.Name] = x => serializationMethod((TypeProperty)x);
             return this;
         }
 
-        public PrintingConfig<TOwner> Clip(Expression<Func<TOwner, string>> stringProperty, int startIndex, int endIndex)
+        public PrintingConfig<TOwner> Clip(Expression<Func<TOwner, string>> stringProperty, int startIndex,
+            int endIndex)
         {
+            var propInfo =
+                ((MemberExpression) stringProperty.Body)
+                .Member as PropertyInfo;
+            if (!Clipper.ContainsKey(propInfo.Name))
+                Clipper.Add(propInfo.Name, null);
+            Clipper[propInfo.Name] =
+                propertyToString => propertyToString.Substring(startIndex, endIndex);
             return this;
         }
 
         public PrintingConfig<TOwner> ExcludeProperty<Propetry>(Expression<Func<TOwner, Propetry>> expression)
         {
+            var propInfo =
+                ((MemberExpression)expression.Body)
+                .Member as PropertyInfo;
+            if (!ExcludePropert.Contains(propInfo.Name))
+                ExcludePropert.Add(propInfo.Name);
             return this;
         }
 
