1883cc8261783048993343ecc6e780bacebeafa5
buritto <ybiz177@gmail.com>
2017-11-27 22:56:09 +0500

    public to private

--- ObjectPrinting/PopertyPrintingConfig.cs
+++ ObjectPrinting/PopertyPrintingConfig.cs
@@ -1,27 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ObjectPrinting
 {
     public class PropertyPrintingConfig<T, TOwner> : IPropertyPrintingConfig<T, TOwner>
     {
         private PrintingConfig<TOwner> config;
+        private HashSet<Type> excludeTypesInConfig;
+        private Dictionary<Type, Func<object, string>> serializationFuncsForDifferentType;
 
-        public PropertyPrintingConfig(PrintingConfig<TOwner> config)
+        public PropertyPrintingConfig(PrintingConfig<TOwner> config, HashSet<Type> excludeTypesInConfig, Dictionary<Type, Func<object, string>>
+            serializationFuncsForDifferentType)
         {
             this.config = config;
+            this.excludeTypesInConfig = excludeTypesInConfig;
+            this.serializationFuncsForDifferentType = serializationFuncsForDifferentType;
         }
 
         public PrintingConfig<TOwner> Config => config;
 
         public PrintingConfig<TOwner> Using(Func<T, string> serializationFun)
         {
-            if (config.ExcludeTypes.Contains(typeof(T)))
+            if (excludeTypesInConfig.Contains(typeof(T)))
                 throw new InvalidOperationException();
-            if (!config.SerializationFuncsForDifferentType.ContainsKey(typeof(T)))
+            if (!serializationFuncsForDifferentType.ContainsKey(typeof(T)))
             {
-                config.SerializationFuncsForDifferentType.Add(typeof(T), null);
+                serializationFuncsForDifferentType.Add(typeof(T), null);
             }
-            config.SerializationFuncsForDifferentType[typeof(T)] = x => serializationFun((T) x);
+            serializationFuncsForDifferentType[typeof(T)] = x => serializationFun((T) x);
             return config;
         }
     }
