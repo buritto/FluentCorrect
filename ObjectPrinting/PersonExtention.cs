6ff59c03e481f04b33e14785dfcd9f0bb60b33d9
buritto <ybiz177@gmail.com>
2017-11-28 21:05:25 +0500

    created extension method for Person

---
+++ ObjectPrinting/PersonExtention.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using ObjectPrinting.Tests;
+
+namespace ObjectPrinting
+{
+    public static class PersonExtention
+    {
+        public static string GetDefaultSerializ(this Person person)
+        {
+            return ObjectPrinter.For<Person>().PrintToString(person);
+        }
+
+        public static string GetDefaultSerializ(this Person person, Func<PrintingConfig<Person>> config)
+        {
+            var pritingConfig = config();
+            return pritingConfig.PrintToString(person);
+        }
+    }
+}
